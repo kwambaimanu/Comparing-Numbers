1fc45cc2c7236d9840352d0e7392cd07f6cc8046
kwambaimanu@gmail.com <kwambaimanu@gmail.com>
2022-05-24 21:36:45 +0300

    Fractal trees

--- Comparing_Numbers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-echo "Enter the value of x"
-read x
-echo "Enter the value of y"
-read y
-[[ $x -gt $y ]] && echo 'X is greater than Y'
-[[ $x -eq $y ]] && echo 'X is equal to Y'
-[[ $x -lt $y ]] && echo 'X is less than Y'
